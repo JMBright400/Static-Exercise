a4b7e8d4dcc5153d9ed366baa16d1b06ef13ebdd
John Bright <jmbright400@gmail.com>
2023-10-11 09:53:51 -0500

    exercise complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double fahrenheit = -40.0;
+            double celsius = TempConverter.FahrenheitToCelcius(fahrenheit);
+            Console.WriteLine($"{fahrenheit} F is equal to {celsius} C");
+
+            celsius = -40.0;
+            fahrenheit = TempConverter.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"{celsius} C is equal to {fahrenheit} F");
+
+            //-40 Celsius and -40 Fahrenheit are equal.
         }
     }
 }
